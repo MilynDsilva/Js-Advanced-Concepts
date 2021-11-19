adcf6a6050a154a8505f7d0df1809125b99450d4
Milyn <milyng.dsilva@gmail.com>
2021-11-19 11:21:59 +0530

    testing file

---
+++ Edit_demo.js
@@ -0,0 +1,106 @@
+let cleanRoom = {
+    return new Promise(function(resolve,reject){
+    resolve('Room is cleaned');
+    //reject('Room is not clean');
+    });
+};
+
+let removeGarbage = function(message){
+    return new Promise(function(resolve,reject){
+    resolve(message+' Garbage has been removed ');
+    });
+};
+
+let winIcecream = function(message){
+    return new Promise(function(resolve,reject){
+    resolve(message+' Won icecream');
+    });
+};
+//race for some
+//all for ;-; all
+Promise.race([cleanRoom(),removeGarbage(),winIcecream()]).then(function(){
+    console.log('All finished');
+})
+
+
+// let cleanRoom = ()=>{
+//     return new Promise(function(resolve,reject){
+//     resolve('Room is cleaned');
+//     //reject('Room is not clean');
+//     });
+// };
+
+// let removeGarbage = message =>{
+//     return new Promise(function(resolve,reject){
+//     resolve(message+' Garbage has been removed ');
+//     });
+// };
+
+// let winIcecream = message =>{
+//     return new Promise(function(resolve,reject){
+//     resolve(message+' Won icecream');
+//     });
+// };
+
+
+
+
+
+
+
+
+
+// cleanRoom().then(function(message){
+//     return removeGarbage(message);
+// }).then(function(message){
+//     return winIcecream(message);
+// }).then(function(message){
+//     console.log('Finished ,Now the '+message);
+// }).catch(function(message){
+//     console.log('Hey , the  '+message);
+// });
+
+// cleanRoom().then(function(resolve){
+//     console.log(resolve);
+// })
+
+// cleanRoom().then(function(resolvereason){
+//     console.log(resolvereason);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let somePromise = new Promise(function(resolve,reject){
+//     isClean = null;
+//     if(isClean){
+//         resolve('yep');
+//     }else{
+//         reject('Nope');
+//     }
+// });
+
+// somePromise.then(function(resolve1){
+//     console.log("room is clean?"+resolve1);
+// }).catch(function(reject1){
+//     console.log('Room is clean??'+reject1);
+// })
--- call-binds-apply.js
+++ call-binds-apply.js
@@ -6,6 +6,18 @@ var addToThis = function(a,b,c) {
 };
 
 console.log(addToThis.call(obj,3,3,2));
+//for apply we need not pass args in an array and pass it
+
+const obj3 = {
+    num:2
+};
+var addToThis = function(a,b,c) {
+    return this.num+a+b+c;  
+};
+
+array1= [2,3,6];
+console.log(addToThis.apply(obj3,array1));
+//
 
 
 const obj1 = {
